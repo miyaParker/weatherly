649b3428b5827b1237fe182bef23cffd8667f4f8
miyaParker <karenziboh@gmail.com>
2020-09-03 03:45:39 -0700

    added service workers

--- script.js
+++ script.js
@@ -217,6 +217,14 @@ const view = {
 }
 
 function init() {
+    if("serviceWorker" in navigator){
+        window.addEventListener("load", function(){
+            navigator.serviceWorker
+                .register("./serviceWorker.js")
+                .then(res=> console.log("service worker is registered"))
+                .catch(err=> console.error("service worker not registered"))
+        })
+    }
     WeatherCard.init()
 }
 init()
